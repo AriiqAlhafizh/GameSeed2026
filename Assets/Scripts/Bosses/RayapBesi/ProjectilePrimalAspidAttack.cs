d0432e6771cb690cecb2e43b94b28117baadacd6
Bintang Khalik <bintangkhaliqq@gmail.com>
2026-06-24 13:19:50 +0700

    added 3 bullet attack to rayapBesi

--- Assets/Scripts/Bosses/RayapBesi/ProjectilePrimalAspidAttack.cs
+++ Assets/Scripts/Bosses/RayapBesi/ProjectilePrimalAspidAttack.cs
@@ -1,16 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
-public class ProjectilePrimalAspidAttack : MonoBehaviour
+public class ProjectilePrimalAspidAttack : BossAttack
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Projectile Settings")]
+    public GameObject projectilePrefab;
+    public float projectileDelay; // 0 untuk shotgun, .5f untuk ada delaynya
+
+    private float facingDirection;
+    public override void Start()
     {
-        
+        base.Start();
+        ActionEvent += ExecuteAttack;
+    }
+    private void OnDisable()
+    {
+        ActionEvent -= ExecuteAttack;
+    }
+    private void ExecuteAttack()
+    {
+        facingDirection = transform.position.x > 0 ? -1f : 1f;
+        StartCoroutine(SpawnProjectile(projectileDelay));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnProjectile(float delay)
     {
-        
+        for (int i = 0; i < 3; i++) 
+        {
+            float projectileRotation = i * 15f * facingDirection;
+            Quaternion rotation = Quaternion.Euler(0, 0, projectileRotation);
+            Instantiate(projectilePrefab, transform.position, rotation);
+            yield return new WaitForSeconds(delay);
+        }
     }
 }
